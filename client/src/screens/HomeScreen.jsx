128be36e763ed57b92e73748406ce02d5aefc4b5
camilla <camillaalves285@gmail.com>
2024-05-14 15:28:17 -0300

    Página Inicial

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, StatusBar, View, ScrollView, FlatList, Image, Text } from 'react-native';
 
 import ProductCard from '../components/ProductCard/ProductCard';
+import ProductDetails from '../components/ProductDetails/ProductDetails';
 
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+
+const Stack = createStackNavigator();
 
 export function HomeScreen() {
 
@@ -21,40 +24,44 @@ export function HomeScreen() {
           console.log(product.attributes);
         });
       })
-      .catch(error => console.error('Erro:', error));
+      .catch(error => console.error('Erro:', error.message));
   }, []);
 
 
   return (
-    <View style={styles.container}>
-      <FlatList
-            data={products}
-            keyExtractor={item => item.id}
-            renderItem={({ item }) => <ProductCard item={item} />}
-            numColumns={2}
-          />
-    </View>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+    <Stack.Navigator>
+      <Stack.Screen name="Produtos" >
+        {props => (
+          <View style={styles.container}>
+            <FlatList
+              data={products}
+              keyExtractor={item => item.id}
+              renderItem={({ item }) => <ProductCard {...props} item={item} />}
+              numColumns={2}
+            />
+          </View>
+        )}
+      </Stack.Screen>
+      <Stack.Screen name="ProductDetails" component={ProductDetails} />
+    </Stack.Navigator>
+  </ScrollView>
   );
 }
 
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+},
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 10,
-    backgroundColor: 'white',
+    // backgroundColor: '#94B0DA',
     justifyContent: 'center',
     alignContent: 'center',
     alignItems:'center'
-  },
-  scrollContent: {
-    flexGrow: 1,
-  },
-  products: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center'
-  },
+  }
 });
 
 
