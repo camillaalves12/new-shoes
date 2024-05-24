2d6a0aa6f222332bc9fbfccedde2e9fa50818156
camilla <camillaalves285@gmail.com>
2024-05-24 08:41:11 -0300

    Modifiquei o componente e está funcionando

--- client/src/screens/FavoritesScreen.jsx
+++ client/src/screens/FavoritesScreen.jsx
@@ -1,29 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import { View, FlatList, StyleSheet } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { StyleSheet, StatusBar, View, ScrollView, FlatList, TouchableOpacity,} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import ProductCard from '../components/ProductCard/ProductCard';
+import { useFocusEffect } from '@react-navigation/native';
 
-import ProductCard from '../components/ProductCard/ProductCard'
 
-
-const FavoritesScreen = () => {
-
+const FavoritesScreen = ({ navigation }) => {
   const [favorites, setFavorites] = useState([]);
+  const isMounted = useRef(false);
+
+  const fetchProducts = async () => {
+    const response = await fetch(`http://192.168.101.189:3000/products`);
+    const data = await response.json();
+    return data.results;
+  };
+
+  const updateFavorites = async () => {
+    const favoriteIds = JSON.parse(await AsyncStorage.getItem('favorites'));
+    const allProducts = await fetchProducts();
+    const favorites = allProducts.filter(product => favoriteIds.includes(product.id));
+    if (isMounted.current) {
+      setFavorites(favorites);
+    }
+  };
 
   useEffect(() => {
-    const getFavorites = async () => {
-      const favorites = await AsyncStorage.getItem('favorites');
-      setFavorites(favorites ? JSON.parse(favorites) : []);
+    isMounted.current = true;
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    updateFavorites();
+  }, []);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      updateFavorites();
+    }, [])
+  );
+
+  const renderItem = ({ item }) => {
+    const handlePress = () => {
+      if (item) {
+        navigation.navigate('Detalhes', { item });
+      }
     };
 
-    getFavorites();
-  }, []);
+    return item ? <ProductCard item={item} onPress={handlePress} /> : null;
+  };
 
   return (
     <View style={styles.container}>
       <FlatList
         data={favorites}
-        keyExtractor={item => item.id ? item.id.toString() : ''}
-        renderItem={({ item }) => <ProductCard item={item} />}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
         numColumns={2}
       />
     </View>
@@ -33,8 +66,9 @@ const FavoritesScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 10,
+    backgroundColor: "#fff",
   }
-  })
+});
 
-
-  export default FavoritesScreen;
+export default FavoritesScreen;
